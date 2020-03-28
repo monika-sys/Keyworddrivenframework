c130793e0cc0879225308935532ad2f72392a0c4
monika-sys <chaudhari.moni789@gmail.com>
2020-03-28 22:43:36 +0530

    Added more keyword

--- src/main/java/com/keywords/Constants.java
+++ src/main/java/com/keywords/Constants.java
@@ -3,10 +3,21 @@ package com.keywords;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import ru.yandex.qatools.ashot.AShot;
 
 public class Constants {
 	public static WebDriver driver;
 	public static Properties prop;
 	public static FileInputStream fis;
+	public static WebElement element;
+	public static Logger log;
+	public static Actions action;
+	public static Alert alert;
+	public static AShot ashot;
 }
